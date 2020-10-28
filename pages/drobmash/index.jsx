f2ba57600ab244bc795cd10e9f16ee78ea50edf7
Sergey Kostin <dr.zad1994@gmail.com>
2020-10-28 17:35:55 +0300

    feat: add Drobmash page

---
+++ pages/drobmash/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { Factory } from "../../components";
+import {
+  Mechanicians,
+  WithoutThought,
+  Locust,
+  ArmoredCar,
+  Сooperation,
+  OnWheels,
+  PatienceAndWork,
+} from "./chapters";
+
+import styles from "./index.module.css";
+
+const Drobmash = () => {
+  return (
+    <Factory>
+      <div className={styles.root}>
+        <Mechanicians />
+        <WithoutThought />
+        <Locust />
+        <ArmoredCar />
+        <Сooperation />
+        <OnWheels />
+        <PatienceAndWork />
+      </div>
+    </Factory>
+  );
+};
+
+export default Drobmash;
