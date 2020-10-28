8ec2a4ff36542b66e15f28937990303bb1db29b8
Sergey Kostin <dr.zad1994@gmail.com>
2020-10-28 19:43:41 +0300

    feat: add Ruspolimet page

---
+++ pages/ruspolimet/chapters/index.js
@@ -0,0 +1,7 @@
+export { default as Heritage } from "./Heritage/Heritage";
+export { default as LastMorning } from "./LastMorning/LastMorning";
+export { default as ForYourself } from "./ForYourself/ForYourself";
+export { default as Magicians } from "./Magicians/Magicians";
+export { default as SpecialOrder } from "./SpecialOrder/SpecialOrder";
+export { default as TankConstruction } from "./TankConstruction/TankConstruction";
+export { default as PeacefulSky } from "./PeacefulSky/PeacefulSky";
