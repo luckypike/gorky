f87a3c397e00a5915c642cd775fc3cb58944a9bb
Sergey Kostin <dr.zad1994@gmail.com>
2020-10-28 16:56:28 +0300

    feat: add Etna page and GunsOfWar component

---
+++ components/GunsOfWar/GunsOfWar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./GunsOfWar.module.css";
+
+const GunsOfWar = ({ title, img, children }) => {
+  return (
+    <div className={styles.root}>
+      <div styles={styles.gun}>
+        {img && <img src={img} alt="img" />}
+        <h2 className={styles.title}>{title}</h2>
+      </div>
+      <div className={styles.desc}>{children}</div>
+    </div>
+  );
+};
+
+GunsOfWar.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+  img: PropTypes.string,
+};
+
+export default GunsOfWar;
---
+++ pages/etna/chapters/index.js
@@ -0,0 +1,6 @@
+export { default as CombatAttachment } from "./CombatAttachment/CombatAttachment";
+export { default as GunsForBattle } from "./GunsForBattle/GunsForBattle";
+export { default as SecondHome } from "./SecondHome/SecondHome";
+export { default as MachinesAndPeople } from "./MachinesAndPeople/MachinesAndPeople";
+export { default as Сreativity } from "./Сreativity/Сreativity";
+export { default as AfterWar } from "./AfterWar/AfterWar";
