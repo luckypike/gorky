cb81062f277fa5e39a0efe552eaff76094ce678f
Sergey Kostin <dr.zad1994@gmail.com>
2020-10-29 15:24:01 +0300

    feat: add Gzhd page and Legend, WarTechnique components

---
+++ components/Legend/Legend.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./Legend.module.css";
+
+const Legend = ({ title, children }) => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.content}>
+        <div className={styles.legend}>ЛЕГЕНДЫ</div>
+        <h2 className={styles.title}>{title}</h2>
+        <div className={styles.desc}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+Legend.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Legend;
---
+++ pages/gzhd/chapters/index.js
@@ -0,0 +1,9 @@
+export { default as StrategicMove } from "./StrategicMove/StrategicMove";
+export { default as Military } from "./Military/Military";
+export { default as Faster } from "./Faster/Faster";
+export { default as NewFrames } from "./NewFrames/NewFrames";
+export { default as LocomotiveOfVictory } from "./LocomotiveOfVictory/LocomotiveOfVictory";
+export { default as Fortresses } from "./Fortresses/Fortresses";
+export { default as Feats } from "./Feats/Feats";
+export { default as ExceptBread } from "./ExceptBread/ExceptBread";
+export { default as Roads } from "./Roads/Roads";
