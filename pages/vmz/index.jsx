1cf116db4c952a1999e510f020b2886c73892f78
Sergey Kostin <dr.zad1994@gmail.com>
2020-10-29 12:55:12 +0300

    feat: add Vmz page

---
+++ pages/vmz/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { Factory } from "../../components";
+import {
+  Modernization,
+  Mobilization,
+  Armor,
+  Masters,
+  CombatConditions,
+  SteelMining,
+  Defense,
+  Character,
+  PeacefulYears
+} from "./chapters";
+
+import styles from "./index.module.css";
+
+const Vmz = () => {
+  return (
+    <Factory>
+      <div className={styles.root}>
+        <Modernization />
+        <Mobilization />
+        <Armor />
+        <Masters />
+        <CombatConditions />
+        <SteelMining />
+        <Defense />
+        <Character />
+        <PeacefulYears />
+      </div>
+    </Factory>
+  );
+};
+
+export default Vmz;
