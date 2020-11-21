4d503655f7b5826d7feecdb98a6766abbf117839
Andy Braga <brg@MMac.local>
2020-11-21 20:30:59 +0300

    Навигация главной страницы

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
 import lax from 'lax.js'
 import Lax from '../components/Lax'
+import cn from 'classnames'
+import Link from 'next/link'
 
 import styles from './index.module.css'
 
@@ -40,6 +43,8 @@ export default function Home () {
     })
   }, [])
 
+  const [selected, setSelected] = useState(false)
+
   return (
     <Lax>
       <div className={styles.container}>
@@ -55,11 +60,69 @@ export default function Home () {
             <div className={styles.overlay} ref={overlayRef} />
           </div>
 
-          <div className={styles.factories} ref={factoriesRef}>
-            <div className={styles.placeholder} />
+          <div className={cn(styles.factories, { [styles.selected]: selected })} ref={factoriesRef}>
+            <Item
+              setSelected={setSelected}
+              href="/gaz"
+              title="Государственный автомобильный завод имени В. М. Молотова"
+              desc="ПАО ГАЗ, ООО «Автомобильный завод ГАЗ»"
+            />
+
+            <Item
+              setSelected={setSelected}
+              href="/sormovo"
+              title="Завод № 112 «Красное Сормово»"
+              desc="ПАО «Завод “Красное Сормово”»"
+            />
+
+          <Item
+            href="/drobmash"
+            title="Выксунский машиностроительный завод дробильно-размольного оборудования"
+            desc="ЗАО «Дробмаш» и АО «Завод корпусов»"
+            setSelected={setSelected}
+          />
+
+            <div className={styles.sep} />
           </div>
         </div>
       </div>
     </Lax>
+  )
+}
+
+Item.propTypes = {
+  href: PropTypes.string,
+  title: PropTypes.string,
+  desc: PropTypes.string,
+  setSelected: PropTypes.func
+}
+
+function Item ({ href, title, desc, setSelected }) {
+  const [active, setActive] = useState(false)
+
+  const handleMouseEnter = e => {
+    setSelected(true)
+    setActive(true)
+  }
+
+  const handleMouseLeave = e => {
+    setSelected(false)
+    setActive(false)
+  }
+
+  return (
+    <div className={cn(styles.factory, { [styles.active]: active })}>
+      <Link href={href}>
+        <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <div className={styles.title}>
+            {title}
+          </div>
+
+          <div className={styles.desc}>
+            {desc}
+          </div>
+        </a>
+      </Link>
+    </div>
   )
 }
