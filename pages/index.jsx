e3a1dc8a6b346e5b57dde542d7abe21fd292f963
Andy Braga <bragovo@gmail.com>
2020-12-03 20:19:50 +0300

    Init Lax only one

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,92 +1,70 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import lax from 'lax.js'
-import Lax from '../components/Lax'
+// import Lax from '../components/Lax'
 import cn from 'classnames'
 import Link from 'next/link'
+
+import useStore from '../stores/useStore'
 
 import styles from './index.module.css'
 
 export default function Home () {
-  const overlayRef = useRef()
-  const factoriesRef = useRef()
-  const introRef = useRef()
+  const appStore = useStore()
 
   useEffect(() => {
-    lax.addDriver('scrollY', () => window.scrollY)
-
-    lax.addElement(overlayRef.current, {
-      scrollY: {
-        opacity: [
-          [0, 'screenHeight / 5'],
-          [0, 0.6]
-        ]
-      }
-    })
-
-    lax.addElement(factoriesRef.current, {
-      scrollY: {
-        opacity: [
-          [0, 'screenHeight / 5'],
-          [0, 1]
-        ]
-      }
-    })
-
-    lax.addElement(introRef.current, {
-      scrollY: {
-        opacity: [
-          [0, 'screenHeight / 5'],
-          [1, 0]
-        ]
-      }
-    })
+    appStore.setLaxInit()
   }, [])
 
-  const [selected, setSelected] = useState(false)
+  const laxInit = useMemo(() => {
+    return appStore.laxInit
+  }, [appStore.laxInit])
+
+  useEffect(() => {
+    if (laxInit) {
+      console.log('WE ARE INIT')
+    }
+  }, [laxInit])
+
+  // const overlayRef = useRef()
+  // const factoriesRef = useRef()
+  // const introRef = useRef()
+
+  // useEffect(() => {
+  //   lax.addDriver('scrollY', () => window.scrollY)
+
+  //   lax.addElement(overlayRef.current, {
+  //     scrollY: {
+  //       opacity: [
+  //         [0, 'screenHeight / 5'],
+  //         [0, 0.6]
+  //       ]
+  //     }
+  //   })
+
+  //   lax.addElement(factoriesRef.current, {
+  //     scrollY: {
+  //       opacity: [
+  //         [0, 'screenHeight / 5'],
+  //         [0, 1]
+  //       ]
+  //     }
+  //   })
+
+  //   lax.addElement(introRef.current, {
+  //     scrollY: {
+  //       opacity: [
+  //         [0, 'screenHeight / 5'],
+  //         [1, 0]
+  //       ]
+  //     }
+  //   })
+  // }, [])
 
   return (
-    <Lax>
-      <div className={styles.container}>
-        <div className={styles.root}>
-          <div className={styles.main}>
-            <div className={styles.introWrapper} ref={introRef}>
-              <div className={styles.intro}>
-                <div className={styles.gradient}>Горький</div>
-                <div className={styles.under}>символ победы</div>
-              </div>
-            </div>
+    <div className={styles.root}>
 
-            <div className={styles.overlay} ref={overlayRef} />
-          </div>
-
-          <div className={cn(styles.factories, { [styles.selected]: selected })} ref={factoriesRef}>
-            <Item
-              setSelected={setSelected}
-              href="/gaz"
-              title="Государственный автомобильный завод имени В. М. Молотова"
-              desc="ПАО ГАЗ, ООО «Автомобильный завод ГАЗ»"
-            />
-
-            <Item
-              setSelected={setSelected}
-              href="/sormovo"
-              title="Завод № 112 «Красное Сормово»"
-              desc="ПАО «Завод “Красное Сормово”»"
-            />
-
-          <Item
-            href="/drobmash"
-            title="Выксунский машиностроительный завод дробильно-размольного оборудования"
-            desc="ЗАО «Дробмаш» и АО «Завод корпусов»"
-            setSelected={setSelected}
-          />
-
-            <div className={styles.sep} />
-          </div>
-        </div>
-      </div>
-    </Lax>
+    </div>
   )
 }
 
