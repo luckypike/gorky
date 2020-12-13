210c17a4c27954618c31c7263e31628c9fb9adc0
Andy Braga <bragovo@gmail.com>
2020-12-13 22:44:37 +0300

    Борский

--- components/MainText/MainText.jsx
+++ components/MainText/MainText.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import cn from 'classnames'
 import PropTypes from 'prop-types'
 
 import styles from './MainText.module.css'
 
 const MainText = ({ title, children }) => {
   return (
-    <div className={styles.root}>
+    <div className={cn(styles.root, { [styles.void]: !children })}>
       <div className={styles.container}>
         <h2 className={styles.title}>
           {title}
         </h2>
 
-        <div className={styles.text}>
-          {children}
-        </div>
+        {children && (
+          <div className={styles.text}>
+            {children}
+          </div>
+        )}
       </div>
     </div>
   )
